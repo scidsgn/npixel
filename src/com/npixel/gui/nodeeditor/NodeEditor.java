cddb60aaac4e3bbace32b685cb0e5cd512532fd0
scintilla4evr <kacper17@outlook.com>
2021-04-03 15:57:14 +0200

    Added node deletion via context menu, basic connection creation

--- src/com/npixel/gui/nodeeditor/NodeEditor.java
+++ src/com/npixel/gui/nodeeditor/NodeEditor.java
@@ -2,11 +2,15 @@ package com.npixel.gui.nodeeditor;
 
 import com.npixel.base.node.Node;
 import com.npixel.base.node.NodeSocket;
+import com.npixel.base.node.NodeSocketType;
 import com.npixel.base.tree.NodeConnection;
 import com.npixel.base.tree.NodeTree;
 import javafx.geometry.VPos;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.input.ContextMenuEvent;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
@@ -14,6 +18,11 @@ import javafx.scene.text.TextAlignment;
 
 public class NodeEditor extends Canvas {
     private final NodeTree tree;
+    private Node activeNode = null;
+    private NodeSocket activeSocket = null;
+
+    private ContextMenu selectedNodeMenu;
+
     private final Double[] lastMousePosition = {0.0, 0.0};
 
     public NodeEditor(NodeTree tree, double width, double height) {
@@ -22,35 +31,105 @@ public class NodeEditor extends Canvas {
         this.tree = tree;
 
         this.setOnMousePressed(this::handleMouseDown);
+        this.setOnMouseReleased(this::handleMouseRelease);
         this.setOnMouseDragged(this::handleDragging);
+        this.setOnContextMenuRequested(this::handleContextMenu);
+
+        setupSelectedNodeMenu();
+    }
+
+    private void setupSelectedNodeMenu() {
+        selectedNodeMenu = new ContextMenu();
+
+        MenuItem deleteItem = new MenuItem("Delete node");
+        deleteItem.setOnAction(event -> {
+            tree.deleteNode(activeNode);
+            activeNode = null;
+
+            render();
+        });
+        MenuItem disconnectItem = new MenuItem("Remove all connections");
+        disconnectItem.setOnAction(event -> {
+            tree.disconnectAll(activeNode);
+            render();
+        });
+
+        selectedNodeMenu.getItems().addAll(deleteItem, disconnectItem);
+    }
+
+    private void handleContextMenu(ContextMenuEvent contextMenuEvent) {
+        if (activeNode != null) {
+            selectedNodeMenu.show(this, contextMenuEvent.getScreenX(), contextMenuEvent.getScreenY());
+        }
     }
 
     private void handleMouseDown(MouseEvent mouseEvent) {
         Node node = hitTest(mouseEvent.getX(), mouseEvent.getY());
 
+        selectedNodeMenu.hide();
+
         if (node != null) {
+            double xOffset = mouseEvent.getX() - node.getX();
+
+            NodeSocket socket = hitTestSocket(node, mouseEvent.getX(), mouseEvent.getY());
+            if (socket != null) {
+                if (
+                        (xOffset < 10 && socket.getType() == NodeSocketType.INPUT) ||
+                        (xOffset >= 140 && socket.getType() == NodeSocketType.OUTPUT)
+                ) {
+                    activeSocket = socket;
+                }
+            }
+
+            activeNode = node;
             tree.bringToFront(node);
+        } else {
+            activeNode = null;
         }
 
         lastMousePosition[0] = mouseEvent.getX();
         lastMousePosition[1] = mouseEvent.getY();
+
+        render();
+    }
+
+    private void handleMouseRelease(MouseEvent mouseEvent) {
+        if (activeSocket != null) {
+            Node node = hitTest(mouseEvent.getX(), mouseEvent.getY());
+            NodeSocket targetSocket = hitTestSocket(node, mouseEvent.getX(), mouseEvent.getY());
+            if (targetSocket != null) {
+                if (node != activeSocket.getParentNode()) {
+                    if (
+                            activeSocket.getType() == NodeSocketType.INPUT &&
+                            targetSocket.getType() == NodeSocketType.OUTPUT
+                    ) {
+                        tree.connect(node, targetSocket.getId(), activeSocket.getParentNode(), activeSocket.getId());
+                    } else if (
+                            activeSocket.getType() == NodeSocketType.OUTPUT &&
+                            targetSocket.getType() == NodeSocketType.INPUT
+                    ) {
+                        tree.connect(activeSocket.getParentNode(), activeSocket.getId(), node, targetSocket.getId());
+                    }
+                }
+            }
+
+            activeSocket = null;
+        }
+        render();
     }
 
     private void handleDragging(MouseEvent mouseEvent) {
         Node node = hitTest(mouseEvent.getX(), mouseEvent.getY());
-        if (node == null) {
-            return;
-        }
 
-        if (mouseEvent.getButton() == MouseButton.PRIMARY) {
+        if (mouseEvent.getButton() == MouseButton.PRIMARY && node != null && activeSocket == null) {
             node.setX(node.getX() + mouseEvent.getX() - lastMousePosition[0]);
             node.setY(node.getY() + mouseEvent.getY() - lastMousePosition[1]);
-
-            render();
         }
 
         lastMousePosition[0] = mouseEvent.getX();
         lastMousePosition[1] = mouseEvent.getY();
+
+        render();
     }
 
     private Node hitTest(double x, double y) {
@@ -67,13 +146,46 @@ public class NodeEditor extends Canvas {
         return hitNode;
     }
 
+    private NodeSocket hitTestSocket(Node node, double x, double y) {
+        if (node == null) {
+            return null;
+        }
+
+        double yOffset = y - node.getY() - 30;
+        int socketIndex = (int)(yOffset / 24);
+
+        if (socketIndex < node.getInputs().size()) {
+            return node.getInputs().get(socketIndex);
+        } else if (socketIndex < node.getInputs().size() + node.getOutputs().size()) {
+            return node.getOutputs().get(socketIndex - node.getInputs().size());
+        }
+
+        return null;
+    }
+
+    private double[] getSocketPosition(NodeSocket socket) {
+        double[] pos = new double[2];
+
+        Node node = socket.getParentNode();
+        pos[0] = node.getX() + ((socket.getType() == NodeSocketType.INPUT) ? 5 : 145);
+        pos[1] = node.getY() + 42;
+
+        if (socket.getType() == NodeSocketType.INPUT) {
+            pos[1] += node.getInputs().indexOf(socket) * 24;
+        } else {
+            pos[1] += node.getOutputs().indexOf(socket) * 24 + node.getInputs().size() * 24;
+        }
+
+        return pos;
+    }
+
     private void renderNode(GraphicsContext ctx, Node node) {
         double xBase = node.getX();
         double yBase = node.getY();
 
         int socketCount = node.getInputs().size() + node.getOutputs().size();
 
-        ctx.setFill(Color.GRAY);
+        ctx.setFill((node == activeNode) ? Color.BLUE : Color.GRAY);
         ctx.fillRect(xBase, yBase, 150, 32 + socketCount * 24);
 
         ctx.setFill(Color.LIGHTGRAY);
@@ -158,5 +270,14 @@ public class NodeEditor extends Canvas {
         for (Node n : tree.getNodes()) {
             renderNode(ctx, n);
         }
+
+        ctx.beginPath();
+        ctx.setStroke(Color.RED);
+        if (activeSocket != null) {
+            double[] pos = getSocketPosition(activeSocket);
+            ctx.moveTo(pos[0], pos[1]);
+            ctx.lineTo(lastMousePosition[0], lastMousePosition[1]);
+        }
+        ctx.stroke();
     }
 }
