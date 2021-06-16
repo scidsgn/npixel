364049edc6c0151c6e2abc06a2de040d92d73d42
scintilla4evr <kacper17@outlook.com>
2021-06-16 17:03:41 +0200

    Small leftover element from testing.

--- src/com/npixel/gui/Main.java
+++ src/com/npixel/gui/Main.java
@@ -15,47 +15,9 @@ import javafx.stage.Stage;
 import java.io.*;
 
 public class Main extends Application {
-    private TabPane tabPane;
-
-    private Tab getDocumentTab(Document doc) {
-        Tab tab = new Tab(doc.getTabName());
-        tab.setClosable(false);
-
-        tab.setContent(new DocumentView(doc));
-
-        doc.on(DocumentEvent.NAMEUPDATED, d -> {
-            tab.setText(doc.getTabName());
-            return null;
-        });
-
-        return tab;
-    }
-
     @Override
     public void start(Stage primaryStage) {
         AppView root = new AppView(primaryStage);
-
-        tabPane = new TabPane();
-        HBox.setHgrow(tabPane, Priority.ALWAYS);
-
-        Button testSave = new Button("test save");
-        testSave.setOnAction(actionEvent -> {
-            // TEST
-            try {
-                File file = new File("test.npxl");
-                file.createNewFile();
-                FileOutputStream fileOS = new FileOutputStream(file, false);
-                DataOutputStream stream = new DataOutputStream(fileOS);
-
-                DocumentWriter documentWriter = new DocumentWriter(stream);
-                //documentWriter.writeDocument(doc);
-
-                stream.flush();
-                stream.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
 
         Scene scene = new Scene(root, 1500, 940);
         scene.getStylesheets().add(getClass().getResource("styles.css").toString());
